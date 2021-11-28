eab1a41050a3fb20dbf97b9c144447902dc9b975
jerrykress <jerrykress.wsj@gmail.com>
2021-11-28 14:18:34 +0000

    Added Speed Up Increment Graph

--- src/graph.py
+++ src/graph.py
@@ -8,22 +8,32 @@ filename = "test_result.png"
 data = pd.read_csv("test_results.txt", sep=" ", header=None)
 data.columns = ["Input Size", "Sum Error", "Percentage", "Seq Time", "Neon Time"]
 data["Speed Up"] = data["Seq Time"] / data["Neon Time"]
+data["Increment"] = (
+    data["Speed Up"] - data.shift(periods=1, axis="index", fill_value=0)["Speed Up"]
+)
 
 fig, axes = plt.subplots(nrows=2, ncols=2)
-plt.tight_layout(pad=3, w_pad=2.0, h_pad=5)
+plt.tight_layout(pad=4, w_pad=4.0, h_pad=5)
 
+################ plot 1 ####################
 axes[0, 0].title.set_text("Error Percentage")
 axes[0, 0].set_ylabel("%")
 data.plot(ax=axes[0, 0], x="Input Size", y="Percentage", kind="line")
 
+################ plot 2 ####################
 axes[0, 1].title.set_text("Execution Time")
 axes[0, 1].set_ylabel("ms")
 data.plot(ax=axes[0, 1], x="Input Size", y=["Seq Time", "Neon Time"], kind="line")
 
+################ plot 3 ####################
 axes[1, 0].title.set_text("Speed Up Factor")
 axes[1, 0].set_ylabel("times")
 data.plot(ax=axes[1, 0], x="Input Size", y="Speed Up", kind="line")
 
+################ plot 4 ####################
+axes[1, 1].title.set_text("Speed Up Increment")
+axes[1, 1].set_ylabel("times")
+data.plot(ax=axes[1, 1], x="Input Size", y="Increment", kind="line")
 
 plt.savefig(filename)
 
